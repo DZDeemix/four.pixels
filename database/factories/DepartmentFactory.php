ff0a930ae0138357bcb48e9adcd45071f6ea6c7e
dmitry <zyablikovdmitry@gmail.com>
2020-09-14 15:05:28 +0400

    added migration model and factory for Department

---
+++ database/factories/DepartmentFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Department;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DepartmentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Department::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => 1,
+            'name' => $this->faker->unique()->sentence,
+            'description' => $this->faker->sentence(20),
+            'logo' => 'app/logo/test.jpg',
+        ];
+    }
+}
---
+++ database/migrations/2020_09_14_094958_create_departments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDepartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('name')->unique();
+            $table->text('description');
+            $table->string('logo');
+            $table->timestamps();
+            $table->foreign('user_id')
+                ->references('id')->on('users')->onDelete('cascade');
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('departments');
+    }
+}
