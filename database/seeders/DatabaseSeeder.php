c3d55b864b60d072653cf4194ce5cc7252495d49
dmitry <zyablikovdmitry@gmail.com>
2020-09-14 15:08:28 +0400

    DatabaseSeeder has been changed for loading initial data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Department;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(15)->create();
+        Department::factory(15)->create();
+        $this->call([
+            TestUser::class,
+        ]);
     }
 }
